8b0f6bef557ba20d4d18ccf9b1212aa95d33953e
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-21 17:03:55 -0400

    remove hardcoded values from scores

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
